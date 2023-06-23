7becfd4ae1cd67bd4f1921d01d97d826294075b8
EdinsonR7 <edinson.rivera@itglobers.com>
2023-06-22 22:41:43 -0500

    refactorizando codigo que causa error

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,6 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-const cors = require("cors");
 
 require("./db.js");
 
@@ -16,13 +15,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 
-const corsOptions = {
-  origin: "https://deploy-pi-dogs-eta.vercel.app/",
-  credentials: true,
-  //access-control-allow-credentials:true,
-  optionSuccessStatus: 200,
-};
-server.use(cors(corsOptions));
+//'http://localhost:3000'
 
 server.use((req, res, next) => {
   res.header(
