103d476d1553238e9e95d7fa81310ee66077af58
EdinsonR7 <edinson.rivera@itglobers.com>
2023-07-05 15:07:19 -0500

    style: modificando landing page

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import image from '../../image/doglanding.png';
 import styles from '../../css/LandingPage.module.css';
 import {Link} from 'react-router-dom'
 
 const LandingPage = () => {
     return (
-        <div className={styles.container}>
-         <img src={image} alt="" /> 
+        <div className={styles.container}> 
           <div className={styles.containerHome}>
           <Link to={'/home'}>HOME</Link>
           <h1>BIENVENIDOS AL MUNDO DOGS</h1>
