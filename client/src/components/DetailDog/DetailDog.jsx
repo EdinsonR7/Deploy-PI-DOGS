91695bfee0eb223dda4cd5c3165e70087ac5ae26
EdinsonR7 <edinson.rivera@itglobers.com>
2023-07-05 16:21:04 -0500

    style: modificando contenedor detail

--- client/src/components/DetailDog/DetailDog.jsx
+++ client/src/components/DetailDog/DetailDog.jsx
@@ -1,49 +1,49 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useParams, } from 'react-router-dom';
-import { getBreedDetail } from '../../redux/actions';
-import styles from '../../css/DetailDog.module.css';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { getBreedDetail } from "../../redux/actions";
+import styles from "../../css/DetailDog.module.css";
 
 const DetailDog = () => {
   const dispatch = useDispatch();
-  const detailBreed = useSelector(state => state.detailBreed);
+  const detailBreed = useSelector((state) => state.detailBreed);
   let { id } = useParams();
 
   useEffect(() => {
     dispatch(getBreedDetail(id));
-      // eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
   return (
     <div className={styles.detailbody}>
-    <div className={styles.detailcard}>
-    <h1> RAZA DETALLADA </h1>
-    <div className={styles.cardbody}>
-    <div className={styles.detailimg}>
-    <img src={detailBreed.image} alt='img'/>
-    </div>
-    <div className={styles.bodytext}>
-    <div>
-    <h1>{detailBreed.breed}</h1>
-    </div>
+      <div className={styles.detailcard}>
+        <h1> RAZA DETALLADA </h1>
+        <div className={styles.cardbody}>
+          <div className={styles.detailimg}>
+            <img src={detailBreed.image} alt="img" />
+          </div>
+          <div className={styles.bodytext}>
+            <div>
+              <h1>{detailBreed.breed}</h1>
+            </div>
 
-    <div className={styles.descriptionbody}>
-    <p>•WEIGHT: {detailBreed.weight} lb</p>
-    <p>•HEIGHT: {detailBreed.height} ft</p>
-    <p>•YEARS OF LIFE: {detailBreed['years of life']}</p>
-    <div className={styles.temptext}>
-    <p>•TEMPERAMENTS:</p>
-    <p>{detailBreed.temperament || 'No tempers'}</p>
-    </div>
-    </div>
-    </div>
-    </div>
-    <div className={styles.buttonbody}>
-    <Link className={styles.buttonBack} to={'/home'}>
-       {'<'}
-    </Link>
-    </div>
-    </div>
+            <div className={styles.descriptionbody}>
+              <p>•WEIGHT: {detailBreed.weight} lb</p>
+              <p>•HEIGHT: {detailBreed.height} ft</p>
+              <p>•YEARS OF LIFE: {detailBreed["years of life"]}</p>
+              <div className={styles.temptext}>
+                <p>•TEMPERAMENTS:</p>
+                <p>{detailBreed.temperament || "No tempers"}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttonbody}>
+          <Link className={styles.buttonBack} to={"/home"}>
+            {"<"}
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
