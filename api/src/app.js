936a00971c4d0d914c3c229834171b33ee7bc25f
EdinsonR7 <edinson.rivera@itglobers.com>
2024-01-03 20:38:17 -0500

    actualizacion de deploy

--- api/src/app.js
+++ api/src/app.js
@@ -18,11 +18,10 @@ server.use(morgan("dev"));
 //'http://localhost:3000'
 
 server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "http://localhost:3001"
-    // "https://deploy-pi-dogs-eta.vercel.app"
-  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  // "https://deploy-pi-dogs-eta.vercel.app"
+  // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
