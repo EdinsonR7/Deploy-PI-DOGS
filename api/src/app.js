a211ae16b7aef39029195f1d96c76aec8e4567a0
EdinsonR7 <edinson.rivera@itglobers.com>
2024-01-03 19:31:20 -0500

    actualizacion

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "http://localhost:3000"
+    "http://localhost:3001"
     // "https://deploy-pi-dogs-eta.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
