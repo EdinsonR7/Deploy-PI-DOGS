5f7f5cffba94fea91b4218c1a2ab531f21285a48
EdinsonR7 <edinson.rivera@itglobers.com>
2024-02-05 17:03:53 -0500

    actualizacion para deploy

--- api/src/db.js
+++ api/src/db.js
@@ -1,12 +1,19 @@
+import pg from "pg";
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DATABASE_URL } =
-  process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}:${DATABASE_URL}/${DB_NAME}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
