88565e3bd4d920b30e766412e76f928d7a16df68
EdinsonR7 <edinson.rivera@itglobers.com>
2024-06-19 15:03:44 -0500

    configurando Url

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-
+const cors = require("cors");
 require("./db.js");
 
 const server = express();
@@ -17,20 +17,23 @@ server.use(morgan("dev"));
 
 //'http://localhost:3000'
 
-server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://deploy-pi-dogs-eta.vercel.app"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     // "https://deploy-pi-dogs-eta.vercel.app"
+//     "http://localhost:3000"
+//   );
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
+
+server.use(cors());
 
 server.use("/", routes);
 
--- api/src/routes/DogsRouter/router.js
+++ api/src/routes/DogsRouter/router.js
@@ -1,52 +1,53 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const { getDogs, detailDog, createDog, deleteDog  }= require('./controllers.js')
+const {
+  getDogs,
+  detailDog,
+  createDog,
+  deleteDog,
+} = require("./controllers.js");
 
-router.get('/', async (req, res) => {
-const { name } = req.query;
-try {
-    const dogs = await getDogs(name)
-    res.status(200).json(dogs)
-} catch (error) {
-    res.status(400).json({error: error.message});
-}
-})
+router.get("/", async (req, res) => {
+  const { name } = req.query;
+  console.log({ name }, "esto es una prueba");
+  try {
+    const dogs = await getDogs(name);
+    res.status(200).json(dogs);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
-router.get('/:id', async (req, res) => {
-  const { id }= req.params;
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
 
   try {
     const dogId = await detailDog(id, getDogs);
-    res.status(200).json(dogId)
-    
+    res.status(200).json(dogId);
   } catch (error) {
     res.status(400).json(error.message);
-    
   }
-})
+});
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { image, breed, temperaments, height, weight, years } = req.body;
 
   try {
-   let newDog = await createDog(image, breed, temperaments, height, weight, years, getDogs)
-  if(newDog) res.send('Dog Creado');
+    let newDog = await createDog(
+      image,
+      breed,
+      temperaments,
+      height,
+      weight,
+      years,
+      getDogs
+    );
+    if (newDog) res.send("Dog Creado");
   } catch (error) {
     res.send(error.message);
   }
 });
 
-
-
-router.delete('/:id', deleteDog);
-	
-
-
-
-
-
-
-
-
+router.delete("/:id", deleteDog);
 
 module.exports = router;
