7c71c219ef4eef709af2f1ac4453450a1924e701
Edinson <riveraedinson043@gmail.com>
2025-03-27 10:30:03 -0500

    Actualizando dependencias

--- api/src/app.js
+++ api/src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 require("./db.js");
 
 const server = express();
-
+//API
 server.name = "API";
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
